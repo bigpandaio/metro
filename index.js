73f514f134346c4124606ee43426a22d8421ce8e
Shai Israel <shai@bigpanda.io>
2014-04-08 15:33:11 +0300

    main entry of the metro

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = require('./lib/metro').Metro;
+
+module.exports = {
+  cache: {},
+  create: function(name, piplines, force) {
+    if (!this.cache[name] || force) {
+      this.cache[name] = new Metro(piplines);
+    }
+
+    return this.cache[name];
+
+  },
+  retrieve: function(name) {
+    if (!name || !this.cache[name]) {
+      return null;
+    }
+
+    return this.cache[name];
+  }
+}
